11dead7f2de532d9a71a07001e2b29d289b0ecdd
Andrey Surzhan <surzhan.a.y@gmail.com>
2018-03-21 22:28:26 -0400

    Aded logging and config

---
+++ src/utils/logging.js
@@ -0,0 +1,17 @@
+const winston = require('winston');
+const config = require('config');
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console({
+            handleExceptions: true,
+            level: config.get('loggingLevel'),
+            timestamp: true,
+            humanReadableUnhandledException: true,
+            colorize: true
+        }),
+    ],
+    exitOnError: false
+});
+
+module.exports = logger;
