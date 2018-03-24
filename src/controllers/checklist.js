0e3fe41ebe92595f5748396c58e2a84d6a16b6b6
Andrey Surzhan <surzhan.a.y@gmail.com>
2018-03-24 18:18:51 -0400

    Added controllers for checklist and item

--- src/controllers/checklist.js
+++ src/controllers/checklist.js
@@ -1,3 +1,29 @@
-/**
- * Created by user on 3/7/18.
- */
+const ChecklistRepo = require('../repositories/checklist');
+const ItemRepo = require('../repositories/item');
+
+module.exports = class ChecklistController {
+
+    constructor() {
+        this.checklistRepo = new ChecklistRepo();
+    }
+
+    async addNewChecklist(checklist) {
+        return await this.checklistRepo.inser(checklist);
+    }
+
+    async getAllByUserId(userId) {
+        return await this.checklistRepo.findAll(userId);
+    }
+
+    async updateChecklist(checklist) {
+        return await this.checklistRepo.update(checklist);
+    }
+
+    async deleteChecklist(id) {
+        let itemRepo = new ItemRepo();
+
+        await itemRepo.deleteAll(id);
+
+        return await this.checklistRepo.delete(id);
+    }
+};
--- src/controllers/item.js
+++ src/controllers/item.js
@@ -1,3 +1,27 @@
-/**
- * Created by user on 3/7/18.
- */
+const ItemRepo = require('../repositories/item');
+
+module.exports = class ItemController {
+
+    constructor() {
+        this.itemRepo = new ItemRepo();
+    }
+
+    async addNewItem(checklist) {
+        return await this.itemRepo.inser(checklist);
+    }
+
+    async getAllByChecklistId(checklistId) {
+        return await this.itemRepo.findAll(checklistId);
+    }
+
+    async updateItem(item) {
+        //TODO: add get translation logic
+        // update items with new translation
+
+        return await this.itemRepo.update(item);
+    }
+
+    async deleteItem(id) {
+        return await this.itemRepo.delete(id);
+    }
+};
