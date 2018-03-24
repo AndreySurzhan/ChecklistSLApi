7cae1acff190e6a2039292940cad866b828ef89b
Andrey Surzhan <surzhan.a.y@gmail.com>
2018-03-24 18:46:24 -0400

    Added google translation controller

--- src/controllers/translation.js
+++ src/controllers/translation.js
@@ -1,3 +1,37 @@
-/**
- * Created by user on 3/7/18.
- */
+const logging = require('../utils/logging');
+const translate = require('google-translate-api');
+
+module.exports = class Translate {
+    constructor(user) {
+        this.user = user;
+    }
+
+    async translateMany(text) {
+        let translations = [];
+
+        for (let i = 0; i < languages.length; i++) {
+            let translation = {};
+            let response;
+
+            try {
+                response = await translate(text, {
+                    to: languages[i]
+                });
+                translation.translation = response.text
+            } catch (error) {
+                logging.error(`Failed to translate "${text}"`)
+                logging.error(error);
+
+                return error;
+            }
+
+            translation.language = languages[i]
+            translation.created = new Date();
+            translation.createdBy = this.user._id
+
+            translations.push(translation);
+        }
+
+        return translations;
+    };
+};
